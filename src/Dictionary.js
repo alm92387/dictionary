b4533f5ad97b6a4184889a86a5469f55f65ddd5c
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-07-02 20:56:28 -0400

    added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,20 @@
 import React, { useState } from "react"
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState ("");
+    function handleResponse(response) {
+     console.log(response);   
+    }
     function search (event) { 
         event.preventDefault();
         alert (`searching for ${keyword} definition`);
+        let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en_US/sunset";
+        axios.get(apiUrl).then(handleResponse)
     }
+
+
     function handleKeywordChange (event)
     {
         setKeyword (event.target.value);
