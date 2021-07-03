9992112a3ec1727abf3eba72257f1f12de8716f7
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-07-02 20:05:41 -0400

    linked github repository to netlify and added form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import Dictionary from "./Dictionary"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">  </header>
+      <header className="App-header"> 
+       </header>
+       <main>
+         <Dictionary />
+       </main>
     </div>
   );
 }
 
-export default App;
+
