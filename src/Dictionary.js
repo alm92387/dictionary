eb94b8cc3a7ca2dcc498e9e9b2e8aca250683700
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-07-02 20:28:53 -0400

    added alert to search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,20 @@
-// import React from "react"
+import React, { useState } from "react"
 import "./Dictionary.css";
 
 export default function Dictionary () {
+    let [keyword, setKeyword] = useState ("");
     function search (event) { 
         event.preventDefault();
-        alert ("searching");
+        alert (`searching for ${keyword} definition`);
+    }
+    function handleKeywordChange (event)
+    {
+        setKeyword (event.target.value);
     }
     return (
         <div className="Dictionary">
             <form onSubmit={search}>
-                <input type="search" />
+                <input type="search" onChange={handleKeywordChange} />
             </form>
         </div>
     )
