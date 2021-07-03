e903d045c92b369cc1046ceaedf2424c24eb5a26
alm92387 <81530707+alm92387@users.noreply.github.com>
2021-07-02 22:08:53 -0400

    added loop for definitions and part of speech

---
+++ src/Meaning.js
@@ -0,0 +1,17 @@
+// import Reeact from "react";
+
+export default function Meaning (props)  {
+return (
+    <div className="Meaning"> 
+    <h3>{props.meaning.partOfSpeech}</h3>
+    {props.meaning.definitions.map(function(definition, index){
+        return (
+            <div key={index}>
+                {definition.definition}
+                {definition.example}
+    </div>
+        );
+    })}
+    </div>
+);
+}
--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import Meaning from "./Meaning";
 
 
 export default function Results (props) {
@@ -6,9 +7,11 @@ export default function Results (props) {
     return <div className="Results"> 
     <h2>{props.results.word} </h2>
     {props.results.meanings.map(function(meaning, index){
-        return meaning.definitions[0].definition;
+        return <div key={index}> 
+        <Meaning meaning={meaning} />
+        </div>;
     })}
-    Hello </div> ;
+     </div> ;
     }else {
         return null;
     }
